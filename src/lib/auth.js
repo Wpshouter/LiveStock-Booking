9de02edc64b8a429efb2258a79d784d4802bc295
Jaber Molla <molla.jaber@gmail.com>
2026-05-01 04:11:09 +0600

    fixing some bugs

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,7 +5,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-// const client = new MongoClient("mongodb+srv://mollajaber_db_user:sBuphqe60lxFGpvf@cluster0.q2xijjg.mongodb.net/?appName=Cluster0");
+
 const client = new MongoClient(process.env.MONGO_URI);
 await client.connect();
 const db = client.db("qurbanihat");
